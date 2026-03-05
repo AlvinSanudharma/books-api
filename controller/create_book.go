d671dad375fc8442f694f410e4fa3096ba592a81
AlvinSanudharma <alvinsanudharma@gmail.com>
2026-03-05 13:47:33 +0800

    Custom Validation

--- controller/create_book.go
+++ controller/create_book.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"net/http"
+	"regexp"
+	"strings"
 
 	"github.com/AlvinSanudharma/books-api/database"
 	"github.com/AlvinSanudharma/books-api/dto"
@@ -20,8 +22,29 @@ func CreateBookController(c *fiber.Ctx) error {
 	validate := validator.New()
 	err = validate.Struct(request)
 	if err != nil {
+		validationErrs := err.(validator.ValidationErrors)
+
+		errRes := make(map[string]string)
+		for _, validationErr := range validationErrs {
+			tag := validationErr.Tag()
+			message := "Unknown error"
+
+			switch tag {
+			case "required":
+				message = "Required"
+			case "min":
+				minimum := validationErr.Param()
+
+				message = "Minimum should be " + minimum
+			}
+
+			re := regexp.MustCompile("([a-z])([A-Z])")
+			snakeCase := re.ReplaceAllString(validationErr.Field(), "${1}_${2}")
+			errRes[strings.ToLower(snakeCase)] = message
+		}
+
 		return c.Status(http.StatusBadRequest).JSON(map[string]any{
-			"error": err.Error(),
+			"error": errRes,
 		})
 	}
 
